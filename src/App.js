259ea2b61525acd5332346cb7050c13494a6e6fd
sungsieon <mari9908@naver.com>
2024-12-12 16:14:19 +0900

    pokedex

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Pokedex from './component/Pokedex';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Pokedex />
+    </>
+  );
+}
+
+export default App;
