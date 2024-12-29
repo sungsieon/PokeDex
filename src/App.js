1a9fc3cccab4d541bb9cc1e02be0e68813ef1e61
sungsieon <mari9908@naver.com>
2024-12-30 06:30:51 +0900

    feat: 검색 필터 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-import Pokedex from './component/Pokedex';
-import Navbar from './component/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Search from "./component/Search";
+import Pokedex from "./component/Pokedex";
+import { useState, useRef } from "react";
 
 function App() {
+  const [changeLanguage, setChangeLanguage] = useState(false);
+  const [changeBright, setChangeBright] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+    if (e.target) {
+      setInputValue(e.target.value);
+    }
+  };
+
+  function toggleLanguage() {
+    setChangeLanguage(!changeLanguage);
+  }
+
+  function toggleBright() {
+    setChangeBright(!changeBright);
+  }
+
   return (
-    <>
-      <Navbar />
-      <Pokedex />
-    </>
+    <div className={changeBright ? "blackBackground" : "whiteBackground"}>
+      <Navbar
+        changeBright={changeBright}
+        toggleLanguage={toggleLanguage}
+        toggleBright={toggleBright}
+        changeLanguage={changeLanguage}
+      />
+      <main className="main">
+        <Search
+          inputValue={inputValue}
+          handleChange={handleChange}
+          changeBright={changeBright}
+        />
+        <Pokedex
+          inputValue={inputValue}
+          changeLanguage={changeLanguage}
+          changeBright={changeBright}
+        />
+      </main>
+    </div>
   );
 }
 
